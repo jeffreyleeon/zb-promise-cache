66e7df0df7d71a4c98765f69cfe6971df79702a1
Jeffrey LEE <jeffrey.lee@zingbox.com>
2018-07-08 09:59:29 -0700

    Add function comment

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 
 const LRU = require("lru-cache");
 
+/** Class representing a promise cache. */
 class PromiseCache {
+  /**
+   * Create a promise cache object.
+   * @param {Promise} promise - A promise that will resolve a value to be cached
+   *                            based on a set of given param(s).
+   * @param {JSON} options - Configurations.
+   */
   constructor(promise, options) {
     this._promise = promise;
     this._options = options || {};
